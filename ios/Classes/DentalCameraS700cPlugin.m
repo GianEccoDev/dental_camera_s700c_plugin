980519344e452c9900140bf445d2bddb5e9815a4
AlmaJPenna <alma.pennacchi@eccosolution.it>
2024-06-06 12:49:40 +0200

    1 post restart

--- ios/Classes/DentalCameraS700cPlugin.m
+++ ios/Classes/DentalCameraS700cPlugin.m
@@ -1,4 +1,28 @@
 #import "DentalCameraS700cPlugin.h"
+#import "CameraView.h"
+#import "AppDelegate.h"
+#import <Flutter/Flutter.h>
+
+
+@interface CameraViewFactory : NSObject <FlutterPlatformViewFactory>
+@property (nonatomic, strong) CameraView *cameraView; // Retain the CameraView instance
+@end
+
+@implementation CameraViewFactory
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _cameraView = [[CameraView alloc] initWithFrame:CGRectZero viewIdentifier:0 arguments:nil];
+    }
+    return self;
+}
+
+- (NSObject<FlutterPlatformView> *)createWithFrame:(CGRect)frame viewIdentifier:(int64_t)viewId arguments:(id)args {
+    return self.cameraView;
+}
+
+@end
 
 @implementation DentalCameraS700cPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -7,14 +31,10 @@
             binaryMessenger:[registrar messenger]];
   DentalCameraS700cPlugin* instance = [[DentalCameraS700cPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+    
+  CameraViewFactory* factory = [[CameraViewFactory alloc] initWithMessenger:[registrar messenger]];
+  [registrar registerViewFactory:factory withId:@"my_uikit_view"];
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
 
 @end
