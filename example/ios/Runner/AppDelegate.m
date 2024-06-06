54cb76578d90cc9db667d0341bc9d6e7df1f87ca
AlmaJPenna <alma.pennacchi@eccosolution.it>
2024-06-06 12:58:22 +0200

    3 post ripristino

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -4,8 +4,17 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     [GeneratedPluginRegistrant registerWithRegistry:self];
+    
+    // Initialize and retain a single CameraView instance
+    cameraViewFactory = [[CameraViewFactory alloc] init];
+    NSObject<FlutterPluginRegistrar> *registrar = [controller.engine registrarForPlugin:@"CameraViewFactory"];
+    [registrar registerViewFactory:cameraViewFactory withId:@"my_uikit_view"];
+    
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    
 }
 
 @end
