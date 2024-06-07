0b6ca10a8e01437bdf286079204954ffdd6adf2c
AlmaJPenna <alma.pennacchi@eccosolution.it>
2024-06-07 09:43:27 +0200

    day 2 prova 1

--- ios/Classes/DentalCameraS700cPlugin.m
+++ ios/Classes/DentalCameraS700cPlugin.m
@@ -1,4 +1,9 @@
 #import "DentalCameraS700cPlugin.h"
+#import "CameraView.h"
+#import "ViewController.h"
+#import "AppDelegate.h"
+#import "GeneratedPluginRegistrant.h"
+#import "Flutter"
 
 @implementation DentalCameraS700cPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -10,12 +15,12 @@
     
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
+//- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+//  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+//    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+//  } else {
+//    result(FlutterMethodNotImplemented);
+//  }
+//}
 
 @end
