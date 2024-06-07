b6e12610654b0a75da87eebf59d67e7313923282
AlmaJPenna <alma.pennacchi@eccosolution.it>
2024-06-07 19:20:00 +0200

    prova 4567

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:dental_camera_s700c_plugin/dental_camera_s700c_view.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:flutter/services.dart';
+import 'package:dental_camera_s700c_plugin/dental_camera_s700c_plugin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,36 +16,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  final _dentalCameraS700cPlugin = DentalCameraS700cPlugin();
+
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: Home());
+  void initState() {
+    super.initState();
+    initPlatformState();
   }
-}
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    try {
+      platformVersion =
+          await _dentalCameraS700cPlugin.getPlatformVersion() ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            systemNavigationBarColor: Colors.white,
-            systemNavigationBarIconBrightness: Brightness.dark,
-            statusBarIconBrightness: Brightness.dark,
-            statusBarBrightness: Brightness.dark,
-          ),
         ),
         body: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const S700cView(),
-                ));
-              },
-              child: const Text('Inizia')),
-        ));
+          child: Text('Running on: $_platformVersion\n'),
+        ),
+      ),
+    );
   }
 }
