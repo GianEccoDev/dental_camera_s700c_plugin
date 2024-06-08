7a7be0143056e02b11ad961cab2ef894764c2229
AlmaJPenna <alma.pennacchi@eccosolution.it>
2024-06-08 10:32:54 +0200

    prove con un nuovo plugin da zero

--- ios/Classes/DentalCameraS700cPlugin.m
+++ ios/Classes/DentalCameraS700cPlugin.m
@@ -1,7 +1,32 @@
 #import "DentalCameraS700cPlugin.h"
+#import "ViewController.h"
+#import "CameraView.h"
 
-@implementation DentalCameraS700cPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+@interface CameraViewFactory : NSObject <FlutterPlatformViewFactory>
+@property (nonatomic, strong) CameraView *cameraView; // Retain the CameraView instance
+@end
+
+@implementation CameraViewFactory
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _cameraView = [[CameraView alloc] initWithFrame:CGRectZero viewIdentifier:0 arguments:nil];
+    }
+    return self;
+}
+
+- (NSObject<FlutterPlatformView> *)createWithFrame:(CGRect)frame viewIdentifier:(int64_t)viewId arguments:(id)args {
+    return self.cameraView;
+}
+
+@end
+
+@implementation DentalCameraS700cPlugin{
+    CameraViewFactory *cameraViewFactory;
+}
+
++(void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"dental_camera_s700c_plugin"
             binaryMessenger:[registrar messenger]];
@@ -11,8 +36,17 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    if ([@"startVideoRecording" isEqualToString:call.method]) {
+        NSLog(@"[DEBUG] startVideoRecording method call received");
+        [cameraViewFactory.cameraView startVideoRecordingWithResult:result];
+        [channel invokeMethod:@"RECORDING_STARTED" arguments:nil];
+    } else if ([@"stopVideoRecording" isEqualToString:call.method]) {
+        NSLog(@"[DEBUG] stopVideoRecording method call received");
+        [cameraViewFactory.cameraView stopVideoRecordingWithResult:result];
+        [channel invokeMethod:@"RECORDING_STOPPED" arguments:nil];
+    } else if ([@"foto_ios" isEqualToString:call.method]) {
+        NSLog(@"[DEBUG] foto_ios method call received");
+        [cameraViewFactory.cameraView capturePhotoWithResult:result];
   } else {
     result(FlutterMethodNotImplemented);
   }
