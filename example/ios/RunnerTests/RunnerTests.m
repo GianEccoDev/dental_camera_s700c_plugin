5182716ac60993d480ef302a7c2ade2b86b0dc83
AlmaJPenna <alma.pennacchi@eccosolution.it>
2024-06-09 10:25:46 +0200

    modifiche

--- example/ios/RunnerTests/RunnerTests.m
+++ example/ios/RunnerTests/RunnerTests.m
@@ -2,12 +2,6 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@import dental_camera_s700c_plugin;
-
-// This demonstrates a simple unit test of the Objective-C portion of this plugin's implementation.
-//
-// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
-
 @interface RunnerTests : XCTestCase
 
 @end
@@ -15,19 +9,8 @@
 @implementation RunnerTests
 
 - (void)testExample {
-  DentalCameraS700cPlugin *plugin = [[DentalCameraS700cPlugin alloc] init];
-
-  FlutterMethodCall *call = [FlutterMethodCall methodCallWithMethodName:@"getPlatformVersion"
-                                                              arguments:nil];
-  XCTestExpectation *expectation = [self expectationWithDescription:@"result block must be called"];
-  [plugin handleMethodCall:call
-                    result:^(id result) {
-                      NSString *expected = [NSString
-                          stringWithFormat:@"iOS %@", UIDevice.currentDevice.systemVersion];
-                      XCTAssertEqualObjects(result, expected);
-                      [expectation fulfill];
-                    }];
-  [self waitForExpectationsWithTimeout:1 handler:nil];
+  // If you add code to the Runner application, consider adding tests here.
+  // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
 }
 
 @end
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
+import 'package:dental_camera_s700c_plugin/dental_camera_s700c_view.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:flutter/services.dart';
-import 'package:dental_camera_s700c_plugin/dental_camera_s700c_plugin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,48 +14,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _dentalCameraS700cPlugin = DentalCameraS700cPlugin();
-
   @override
-  void initState() {
-    super.initState();
-    initPlatformState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
   }
+}
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _dentalCameraS700cPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            systemNavigationBarColor: Colors.white,
+            systemNavigationBarIconBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.dark,
+            statusBarBrightness: Brightness.dark,
+          ),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
-    );
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const S700cView(),
+                ));
+              },
+              child: const Text('Inizia')),
+        ));
   }
 }
